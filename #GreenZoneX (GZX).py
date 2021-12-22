d28fc99ac72cc20d1e01fd6b76cbf28a98a0db71
Tazhmin Williams <59209802+TazhminW@users.noreply.github.com>
2021-12-22 16:45:34 -0500

    GreenZoneX INFO (python)

---
+++ #GreenZoneX (GZX).py
@@ -0,0 +1,19 @@
+#GreenZoneX (GZX)
+
+import urllib.request
+
+#logo attached
+
+url = urllib.request.urlopen('https://i.imgur.com/qbsdIjC.png')
+
+#Issuer: rNgsoCk6mjBq5jcqitBpg1gdfYhKajXsM2
+
+#Twitter: GreenZone__ // https://twitter.com/GreenZone__?s=20
+
+#Telegram: https://t.me/greenzoneeechat
+
+#Discord: https://discord.gg/T3GjF5E9nd
+
+#Facebook: GreenZone_llc // https://www.facebook.com/GreenZoneRecycleToday
+
+#Domain: www.GreenZone.eco
